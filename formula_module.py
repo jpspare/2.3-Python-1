fa3d116276d9b98cd8d1af8ab77b9e94ff3c4102
Joshua Spare <jpspare@gmail.com>
2023-04-17 20:45:19 -0600

    first commit

---
+++ formula_module.py
@@ -0,0 +1,21 @@
+
+# Function 1 - Calculate sqft
+def find_house_area(length, width):
+    """Given the inputs of length and width, function returns the area of the house (or any rectangle)"""
+    area = length * width
+    return area
+
+# Function 2 - Calculate Circumference of a Circle
+
+def find_circle_circ(radius):
+    """
+    Function returns the circumference of a circle given an input radius, including
+    importing the value of pi from math.
+    """
+    from math import pi
+    circ = 2 * pi * radius
+    return circ
+
+
+
+
